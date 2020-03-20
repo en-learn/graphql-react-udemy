db5699a67e68181c23bf503c48f4137065b65780
Elias Norrby <elias.norrby@gmail.com>
2020-03-20 16:22:01 +0100

    L50: Refetching Queries

--- lyrical-graphql/client/components/SongCreate.js
+++ lyrical-graphql/client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import { Link, hashHistory } from 'react-router'
+import query from '../queries/fetchSongs'
 
 class SongCreate extends Component {
   constructor(props) {
@@ -13,11 +14,14 @@ class SongCreate extends Component {
 
   onSubmit(event) {
     event.preventDefault()
-    this.props.mutate({
-      variables: {
-        title: this.state.title,
-      },
-    }).then(() => hashHistory.push('/'))
+    this.props
+      .mutate({
+        variables: {
+          title: this.state.title,
+        },
+        refetchQueries: [{ query }],
+      })
+      .then(() => hashHistory.push('/'))
   }
 
   render() {
--- lyrical-graphql/client/components/SongList.js
+++ lyrical-graphql/client/components/SongList.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 import { Link } from 'react-router'
+import query from '../queries/fetchSongs'
 
 const SongList = ({ data }) => {
   const renderSongs = () =>
@@ -15,23 +15,12 @@ const SongList = ({ data }) => {
     <div>'Loading...'</div>
   ) : (
     <div>
-    <ul className="collection">{renderSongs()}</ul>
-      <Link to="/songs/new"
-        className="btn-floating btn-large red right"
-      >
+      <ul className="collection">{renderSongs()}</ul>
+      <Link to="/songs/new" className="btn-floating btn-large red right">
         <i className="material-icons">add</i>
       </Link>
     </div>
   )
 }
-
-const query = gql`
-  {
-    songs {
-      id
-      title
-    }
-  }
-`
 
 export default graphql(query)(SongList)
