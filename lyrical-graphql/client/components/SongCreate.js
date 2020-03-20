9a692a9adbe09fb473d89bf83c0a68c47107cfdc
Elias Norrby <elias.norrby@gmail.com>
2020-03-20 16:04:42 +0100

    L47: Defining Query Variables in React

--- lyrical-graphql/client/components/SongCreate.js
+++ lyrical-graphql/client/components/SongCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
 class SongCreate extends Component {
@@ -11,6 +12,11 @@ class SongCreate extends Component {
 
   onSubmit(event) {
     event.preventDefault()
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      },
+    })
   }
 
   render() {
@@ -30,11 +36,11 @@ class SongCreate extends Component {
 }
 
 const mutation = gql`
-  mutation {
-    addSong(title:) {
-      // ???
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
     }
   }
 `
 
-export default SongCreate
+export default graphql(mutation)(SongCreate)
