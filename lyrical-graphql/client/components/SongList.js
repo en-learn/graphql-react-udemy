7efd2a85a743e91838bcc807e6b985cca94f26ad
Elias Norrby <elias.norrby@gmail.com>
2020-03-20 08:48:51 +0100

    L38: GQL Queries in React

--- lyrical-graphql/client/components/SongList.js
+++ lyrical-graphql/client/components/SongList.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import gql from 'graphql-tag'
 
 export const SongList = () => {
   return <div>SongList</div>
 }
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`
