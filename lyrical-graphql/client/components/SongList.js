5b3cb7025ffa4202f927595067efc3a5ba8c4a31
Elias Norrby <elias.norrby@gmail.com>
2020-03-20 16:11:34 +0100

    L48: Navigating on Successful Mutation

--- lyrical-graphql/client/components/SongList.js
+++ lyrical-graphql/client/components/SongList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 
 const SongList = ({ data }) => {
   const renderSongs = () =>
@@ -13,7 +14,14 @@ const SongList = ({ data }) => {
   return data.loading ? (
     <div>'Loading...'</div>
   ) : (
+    <div>
     <ul className="collection">{renderSongs()}</ul>
+      <Link to="/songs/new"
+        className="btn-floating btn-large red right"
+      >
+        <i className="material-icons">add</i>
+      </Link>
+    </div>
   )
 }
 
