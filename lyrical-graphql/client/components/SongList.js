b7d059c14674edd3fd35e28f2637003f651e9f0b
Elias Norrby <elias.norrby@gmail.com>
2020-03-20 09:04:09 +0100

    L40: Handling Pending Queries

--- lyrical-graphql/client/components/SongList.js
+++ lyrical-graphql/client/components/SongList.js
@@ -2,9 +2,10 @@ import React from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
-const SongList = (props) => {
-  console.log(props)
-  return <div>SongList</div>
+const SongList = ({ data }) => {
+  const renderSongs = () =>
+    data.songs.map(song => <li>{song.title}</li>)
+  return <div>{data.loading ? 'Loading...' : renderSongs()}</div>
 }
 
 const query = gql`
